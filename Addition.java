91e2801f0a81e2d9f3266dc62d6e9879829f9131
Nithya-Burre <burrenithya16@gmail.com>
2024-11-06 12:20:08 +0530

    added

---
+++ Addition.java
@@ -0,0 +1,12 @@
+//second java program for addition of two numbers
+import java.lang.*;
+public class Addition 
+{
+public static void main(String args[])
+{
+int a=10;
+int b=20;
+int c=a+b;
+System.out.println("addition of two numbers="+c);
+}
+}
